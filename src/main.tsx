9afc5ce3abde3ef349a8aebb83fd433779a0185d
posthearts <posthearts25@gmail.com>
2025-02-02 00:42:55 +0100

    remove GoogleOAuthProvider and add Vercel rewrites configuration

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,6 @@ import Editor from './LetterEditor.tsx';
 import AuthPage from './pages/AuthPage.tsx';
 import NotFoundPage from './pages/NotFoundPage.tsx';
 import { createBrowserRouter, RouterProvider } from 'react-router';
-import { GoogleOAuthProvider } from "@react-oauth/google";
 import { UserProvider } from './context/UserContext';
 import ProtectedRoute from './components/ProtectedRoute.tsx';
 
@@ -23,10 +22,8 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <GoogleOAuthProvider clientId='416493180017-l11607h10j781kmqdktu3bnru11h3srk.apps.googleusercontent.com'>
-      <UserProvider>
+ <UserProvider>
         <RouterProvider router={router} />
       </UserProvider>
-    </GoogleOAuthProvider>
   </StrictMode>,
 );
