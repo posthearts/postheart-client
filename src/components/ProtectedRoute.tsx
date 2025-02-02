2b13823c922b8eb2d433f37b9cf47da287928c0a
posthearts <posthearts25@gmail.com>
2025-02-02 01:18:55 +0100

    feat: enhance ProtectedRoute to restore user state from localStorage

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,11 +1,21 @@
 import { Navigate } from 'react-router';
 import { useUser } from '../context/UserContext';
-
-import { ReactNode } from 'react';
+import { ReactNode, useEffect } from 'react';
 
 export default function ProtectedRoute({ children }: { children: ReactNode }) {
     const userContext = useUser();
-    const user = userContext ? userContext.user : null;
+    const user = userContext?.user;
+
+    useEffect(() => {
+        if (!user) {
+            const token = localStorage.getItem('token');
+            const name = localStorage.getItem('name');
+            const profile_picture = localStorage.getItem('profile_picture');
+            if (token && name && profile_picture) {
+                userContext?.setUser({ token, name, profile_picture });
+            }
+        }
+    }, [user, userContext]);
 
     if (!user || !user.token) {
         return <Navigate to="/auth" />;
