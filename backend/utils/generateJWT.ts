6c9d9b9638bef12fa3915cd54a678ac31fe0d73e
posthearts <posthearts25@gmail.com>
2025-02-03 22:48:25 +0100

    test api

--- backend/utils/generateJWT.ts
+++
@@ -1,5 +0,0 @@
-import jwt from 'jsonwebtoken';
-
-export const generateJWT = (user: { _id: string }): string => {
-  return jwt.sign({ id: user._id }, process.env.JWT_SECRET as string, { expiresIn: '1h' });
-};
--- backend/utils/generateShareableLink.ts
+++
@@ -1,7 +0,0 @@
-import crypto from 'crypto';
-
-const generateShareableLink = (): string => {
-  return crypto.randomBytes(16).toString('hex');
-};
-
-export default generateShareableLink;
