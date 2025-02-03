88e379b6b60c4f474b0e8e0aea5cb594787a8f25
posthearts <posthearts25@gmail.com>
2025-02-03 23:12:17 +0100

    fix letter display

--- src/LetterEditor.tsx
+++ src/LetterEditor.tsx
@@ -1,23 +1,25 @@
-import NavBar from "./components/NavBar.tsx";
-import LetterInput from "./components/LetterInput/LetterInput.tsx";
-import LetterView from "./components/LetterView/LetterView.tsx";
+import NavBar from "./components/NavBar";
+import LetterInput from "./components/LetterInput/LetterInput";
+import LetterView from "./components/LetterView/LetterView";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { LettersProvider } from "./context/lettersContext.tsx";
+import { LettersProvider } from "./context/lettersContext";
 
 const queryClient = new QueryClient();
 
 function Editor() {
-  return <QueryClientProvider client={queryClient}>
-<LettersProvider>
-    <div className="flex h-screen w-screen overflow-hidden bg-backgrounds-canvas">
-      <NavBar />
-      <div className="canvas grow grid grid-cols-[1fr_minmax(664px,_1fr)] items-end p-2 gap-4">
-        <LetterInput />
-        <LetterView />
-      </div>
-    </div>
-  </LettersProvider>
-</QueryClientProvider>
+  return (
+    <QueryClientProvider client={queryClient}>
+      <LettersProvider>
+        <div className="flex h-screen w-screen overflow-hidden bg-backgrounds-canvas">
+          <NavBar />
+          <div className="canvas grow grid grid-cols-[1fr_minmax(664px,_1fr)] items-end p-2 gap-4">
+            <LetterInput />
+            <LetterView />
+          </div>
+        </div>
+      </LettersProvider>
+    </QueryClientProvider>
+  );
 }
 
-export default Editor
+export default Editor;
